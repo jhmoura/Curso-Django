80bca517f8f38b291b96638643565ed068ebe044
Julio H Moura <juliohmoura@pm.me>
2021-11-15 11:36:16 -0300

    002

--- app/views.py
+++ app/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'curso': 'Programação WEB com Django Framework', 
+        'outro': 'Django é TOP!'
+    }
+    return render(request, 'index.html', context)
+
+def contato(request):
+    return render(request, 'contato.html')
+
