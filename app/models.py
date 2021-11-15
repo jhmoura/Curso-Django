484bd37c2ca837acfea7559245f96f422a4dd357
Julio H Moura <juliohmoura@pm.me>
2021-11-15 16:17:13 -0300

    0004

--- app/models.py
+++ app/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
 class Produto(models.Model):
-    nome = models.
+    nome = models.CharField(("Nome"), max_length=50)
+    preco = models.DecimalField(("Preço"), max_digits=12, decimal_places=2)
+    estoque = models.IntegerField(("Quantidade em estoque"))
+    
+    def __str__(self):
+        return self.nome
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    
+    def __str__(self):
+        return self.nome
--- app/views.py
+++ app/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from .models import Produto
 
 def index(request):
+    produtos = Produto.objects.all()
     context = {
         'curso': 'Programação WEB com Django Framework', 
-        'outro': 'Django é TOP!'
+        'outro': 'Django é TOP!',
+        'produtos': produtos,
     }
     return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
 
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+    context = {'produto': prod}
+    return render(request, 'produto.html', context)
